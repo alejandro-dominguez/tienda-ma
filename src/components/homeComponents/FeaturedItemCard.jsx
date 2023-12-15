6c68213baf3e8dedabe4cbd4cc6b17a09f6dbbe3
alejandro-dominguez <alejandro.dominguez1@live.com>
2023-12-15 20:51:17 -0300

    finishing featured-section desktop styles

--- src/components/homeComponents/FeaturedItemCard.jsx
+++ src/components/homeComponents/FeaturedItemCard.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import { FaPlusCircle } from 'react-icons/fa';
+import { shortenText } from '../../utilities';
 
 const FeaturedItemCard = ({ product }) => {
     return (
         <div className='flex flex-col items-start justify-center py-5 md:px-8 bg-rose-300/70 rounded gap-3 shadow'>
             <h3 className='text-white font-bold drop-shadow-sm tracking-wide'>
-                {`${product.brand} ${product.name}`}
+                {shortenText((`${product.brand} ${product.name}`), 21)}
             </h3>
-            <div className='w-44'>
+            <div className='md:w-48 aspect-square object-cover'>
                 <img
                     src={product.img}
                     alt={product.name}
                     className='block w-full rounded drop-shadow-sm'
                 />
             </div>
-            <button
-                type='button'
-                className='mt-1 px-5 py-2 bg-teal-600/95 text-white text-[.9rem] rounded-md shadow-sm
-                transition-colors ease-in-out duration-200 hover:bg-rose-600 focus:bg-rose-600'
-                onClick=''
-            >
-                <span className='block drop-shadow tracking-wide text-[1.025rem]'>
-                    Ver más
-                </span>
-            </button>
+            <div className='flex w-full justify-between items-center'>
+                <button
+                    type='button'
+                    className='mt-1 px-5 py-2 bg-teal-600/95 text-white text-[.9rem] rounded-md shadow-sm
+                    transition-colors ease-in-out duration-200 hover:bg-rose-600 focus:bg-rose-600'
+                    onClick=''
+                >
+                    <span className='block drop-shadow tracking-wide text-[1.025rem]'>
+                        Ver más
+                    </span>
+                </button>
+                <button
+                    type='button'
+                    className='pr-1 text-white drop-shadow-sm transition-colors hover:text-[#ff2045f2] focus:text-[#ff2045f2]'
+                >
+                    <FaPlusCircle className='text-2xl'/>
+                </button>
+
+            </div>
         </div>
     )
 };
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,19 +1,27 @@
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { useGetFirebaseData } from '../customHooks';
+import { FeaturedItemCard } from '../components/homeComponents';
 
 const ItemListContainer = () => {
     const { categoryId } = useParams()
-
-    useEffect(() => {
-        console.log(categoryId)
-    }, [])
-    
+    const [data, error, loading] = useGetFirebaseData(categoryId)
 
     return (
-        <div className=''>
-
-        </div>
+        <>
+        {(data.length && !loading && !error) ?
+            <section
+                id='featured-section'
+                className='w-full grid place-items-center md:px-10 py-10 bg-rose-300/70'
+            >
+                <div className='w-full flex md:flex-row flex-col items-center md:justify-between justify-center md:px-10 py-8
+                bg-rose-500/95 rounded-lg shadow-md'>
+                    {data.map(product => {
+                        return <FeaturedItemCard product={product} />
+                    })}
+                </div>
+            </section>
+        : null}
+        </>
     )
 };
 
