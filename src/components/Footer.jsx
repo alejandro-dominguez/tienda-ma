5ca03492c0b5ac1631c5399a7d60128b653fe811
alejandro-dominguez <alejandro.dominguez1@live.com>
2023-12-25 17:39:21 -0300

    footer deliveries info added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,31 +4,43 @@ import { BiSolidCaretUpCircle } from 'react-icons/bi';
 import { PiInstagramLogoFill } from 'react-icons/pi';
 import { BsFillTelephoneFill } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
+import { FaTruck } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className='w-full grid grid-cols-1 md:grid-cols-3 py-6 px-4 gap-5 md:gap-0 md:px-10 bg-white z-10'>
+        <footer className='w-full grid grid-cols-1 lg:grid-cols-4 py-6 px-4 gap-5 md:gap-0 md:px-10 bg-white z-10'>
             <div className='flex flex-col items-start justify-start'>
                 <h3 className='font-Raleway font-black text-xl text-zinc-800'>
                     Contáctanos
                 </h3>
                 <div className='flex items-center gap-2 mt-2'>
                     <BsFillTelephoneFill className='text-[1.3rem] text-zinc-900' />
-                    <span className='font-bold font-Lato drop-shadow-sm'>
+                    <span className='font-bold font-Lato drop-shadow-sm text-sm'>
                         3416 630 000
                    </span>
                 </div>
                 <div className='flex items-center gap-2 mt-1'>
                     <MdEmail className='text-[1.43rem] text-zinc-900' />
-                    <span className='font-bold font-Lato drop-shadow-sm'>
+                    <span className='font-bold font-Lato drop-shadow-sm text-sm'>
                         contacto.tiendama@gmai.com
                     </span>
                 </div>
                 <div className='flex items-center gap-2 mt-1'>
                     <PiInstagramLogoFill className='text-2xl text-zinc-900' />
-                    <span className='font-bold font-Lato drop-shadow-sm'>
+                    <span className='font-bold font-Lato drop-shadow-sm text-sm'>
                         @tiendama_rosario
                     </span>
+                </div>
+            </div>
+            <div className='flex flex-col items-start justify-end mt-0 md:mt-4 lg:mt-0'>
+                <h3 className='font-Raleway font-black text-base text-zinc-800'>
+                    Zona de Envíos:
+                </h3>
+                <div className='flex items-center gap-2 mt-1'>
+                    <FaTruck className='text-[1.3rem] text-zinc-900' />
+                    <span className='font-bold font-Lato drop-shadow-sm text-sm'>
+                        Rosario y Gran Rosario
+                   </span>
                 </div>
             </div>
             <BiSolidCaretUpCircle
--- src/components/homeComponents/PaymentsSection.jsx
+++ src/components/homeComponents/PaymentsSection.jsx
@@ -11,8 +11,8 @@ const PaymentsSection = () => {
         >
             <div className='grid place-items-center font-Raleway'>
                 <div className='flex items-end gap-6'>
-                    <BiSolidBank className='block text-7xl text-red-500/50 drop-shadow-sm' />
-                    <BiSolidCreditCardAlt className='block text-7xl text-red-500/50 -mb-1 drop-shadow-sm' />
+                    <BiSolidBank className='block text-7xl text-red-500/50 drop-shadow' />
+                    <BiSolidCreditCardAlt className='block text-7xl text-red-500/50 -mb-1 drop-shadow' />
                 </div>
                 <span className='text-center text-sm tracking-wide leading-[1.3rem] drop-shadow'>
                     <span className='font-black text-sm drop-shadow-none mb-1'>
@@ -22,7 +22,7 @@ const PaymentsSection = () => {
                 </span>
             </div>
             <div className='grid place-items-center font-Raleway'>
-                <FaMapMarkerAlt className='block text-[3.5rem] text-red-500/50 drop-shadow-sm' />
+                <FaMapMarkerAlt className='block text-[3.5rem] text-red-500/50 drop-shadow' />
                 <span className='text-center text-sm tracking-wide leading-[1.3rem] drop-shadow'>
                     <span className='font-black text-sm drop-shadow-none mb-1 mt-2'>
                         Envíos a Riosario y Gran Rosario
@@ -31,7 +31,7 @@ const PaymentsSection = () => {
                 </span>
             </div>
             <div className='grid place-items-center font-Raleway'>
-                <FaMoneyBill1Wave className='block text-[4rem] text-red-500/50 drop-shadow-sm' />
+                <FaMoneyBill1Wave className='block text-[4.15rem] text-red-500/50 drop-shadow' />
                 <span className='text-center text-sm font-black tracking-wide'>
                     Descuentos en efectivo
                 </span>
