d9f644d15b879153ac4742f7a6899f9a9f6fd9c0
alejandro-dominguez <alejandro.dominguez1@live.com>
2024-01-03 15:07:47 -0300

    form styles and small functionalities done

--- src/components/cartComponents/CartModalForm.jsx
+++ src/components/cartComponents/CartModalForm.jsx
@@ -2,9 +2,11 @@ import { useContext, useState } from 'react';
 import { ShopContext } from '../../contexts/shopContext';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { BiSolidShare } from 'react-icons/bi';  
+import { Toaster, toast } from 'sonner';
 
 const CartModalForm = ({ showForm, setShowForm }) => {
-    const {products, calculateCartTotal, emptyCart} = useContext(ShopContext)
+    const { products, emptyCart } = useContext(ShopContext)
     const navigate = useNavigate()
     const {
         handleSubmit,
@@ -12,13 +14,26 @@ const CartModalForm = ({ showForm, setShowForm }) => {
     } = useForm()
 
     const onSubmit = handleSubmit(() => {
-        reset()
+        toast.success(
+            'Orden enviada',
+            {
+                description: 'Nos comunicamos en la brevedad una vez confirmado el stock.',
+                duration: 5000,
+                position: 'bottom-center',
+            }
+        )
+        setTimeout(() => {
+            reset()
+            emptyCart()
+            navigate('/')
+        }, 6000)
     })
 
     return (
         <>
         {
             showForm && products.length ?
+                <>
                 <form
                     className='md:w-[62.25%] mt-4 pt-3 pb-6 px-6 sm:px-10 shadow-sm bg-white/70'
                     onSubmit={onSubmit}
@@ -73,9 +88,21 @@ const CartModalForm = ({ showForm, setShowForm }) => {
                             </div>
                         </div>
                     </div>
-                    <small className='block ml-1 mt-3 font-bold tracking-wider'>
-                        Costo de envío a convenir.
-                    </small>
+                    <div className='flex items-center justify-between mt-2'>
+                        <small className='block font-bold tracking-wider'>
+                            Costo de envío a convenir *
+                        </small>
+                        <button
+                            type='button'
+                            className='grid place-items-center drop-shadow-sm'
+                            onClick={() => setShowForm(false)}
+                        >
+                            <BiSolidShare className='block text-red-300 text-2xl' />
+                            <small className='block font font-Raleway font-bold tracking-wide -mt-1'>
+                                Volver    
+                            </small>
+                        </button>
+                    </div>
                     <button
                         type='submit'
                         className='grid place-items-center py-[.625rem] px-6 w-full mt-3 bg-zinc-900 rounded-md transition-colors
@@ -86,6 +113,13 @@ const CartModalForm = ({ showForm, setShowForm }) => {
                         </span>
                     </button>
                 </form>
+                <Toaster
+                    richColors
+                    toastOptions={{
+                        className: 'text-center',
+                    }}
+                />
+                </>
             : 
                 null
         }
