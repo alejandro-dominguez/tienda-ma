2338329084c84fc1c3341d4e7fab209aa8f7aefd
alejandro-dominguez <alejandro.dominguez1@live.com>
2024-01-30 20:55:01 -0300

    error page and disabled site page done

--- src/components/DisabledSite.jsx
+++ src/components/DisabledSite.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import logo from '../assets/logo-header.svg';
 
 const DisabledSite = () => {
     return (
-        <main className='w-full flex flex-col mt-28 min-h-[100svh]'>
-            DisabledSite
+        <main className='w-full grid place-items-start mt-32 min-h-[100svh] px-4 md:px-10'>
+            <div className='grid place-items-center gap-5 justify-self-center'>
+                <h1 className='font-Raleway font-bold text-[1.75rem] md:text-3xl drop-shadow-sm leading-8'>
+                    Por el momento el sitio se encuentra en mantenimiento.
+                </h1>
+                <p className='font-bold leading-[1.375rem]'>
+                    Nos encontramos administrando nuestros servidores.
+                    <br />
+                    Lamentamos mucho el inconveniente.
+                    <br />
+                    Por favor, intenta regresar más tarde.
+                </p>
+            </div>
         </main>
     )
 };
