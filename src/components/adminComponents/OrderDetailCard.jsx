6394a98fa6fce4381db98564f60f19acf15ed274
alejandro-dominguez <alejandro.dominguez1@live.com>
2024-01-30 02:03:11 -0300

    admin orders component done

--- src/components/adminComponents/OrderDetailCard.jsx
+++ src/components/adminComponents/OrderDetailCard.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import AdminOrderProduct from './adminItems/AdminOrderProduct';
 
 const OrderDetailCard = ({ order }) => {
-    console.log(order);
     return (
         <div className='flex flex-col items-start justify-start w-full bg-white rounded-md p-5
         drop-shadow-sm shadow-sm mt-4 mb-20 gap-2'>
             <h1 className='font-bold font-Raleway text-[1.05rem] drop-shadow-sm'>
                 Orden de {order.clientFullName}:
             </h1>
+            <div className='grid place-items-start grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-2'>
+                {
+                    order.orderProducts.map((product, i) => {
+                        return <AdminOrderProduct product={product} key={i}/>
+                    })
+                }
+            </div>
             <span className='font-bold flex gap-1 items-center'>
                 Dirección:
                 <span className='font-normal text-sm mt-[.1rem]'>
---
+++ src/components/adminComponents/adminItems/AdminOrderProduct.jsx
@@ -0,0 +1,38 @@
+import numberFormater from '../../../utilities/numberFormater';
+
+const AdminOrderProduct = ({ product }) => {
+    return (
+        <div className='grid place-items-start gap-[.15rem]'>
+            <h3 className='font-Raleway font-black drop-shadow-sm tracking-wide text-[.85rem]'>
+                {product.brand} {product.name}
+            </h3>
+            <div className='w-[4.5rem] drop-shadow mt-1'>
+                <img
+                    src={product.img}
+                    alt={product.name}
+                    className='block w-full rounded drop-shadow-sm aspect-square object-cover'
+                />
+            </div>
+            <span className='text-[.835rem] font-black tracking-wide mt-1'>
+                Cantidad: {product.quantity}
+            </span>
+            <span className='text-[.835rem] font-black tracking-wide'>
+                Precio unitario: {numberFormater(product.price)}
+            </span>
+            {
+                product.selectedSize ?
+                    <span className='text-[.835rem] font-black tracking-wide -mt-[.2rem]'>
+                        Talle: {product.selectedSize}
+                    </span>
+                : product.selectedAdultSize ?
+                    <span className='text-[.835rem] font-black tracking-wide -mt-[.2rem]'>
+                        Talle: {product.selectedAdultSize}
+                    </span>
+                :
+                    null
+            }
+        </div>
+    )
+};
+
+export default AdminOrderProduct;
