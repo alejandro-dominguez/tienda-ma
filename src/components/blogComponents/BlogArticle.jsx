4e5ca672d5d15a173778e266229b55d292efba9d
alejandro-dominguez <alejandro.dominguez1@live.com>
2024-03-11 18:14:38 -0300

    fixed BlogArticle title styling

--- src/components/blogComponents/BlogArticle.jsx
+++ src/components/blogComponents/BlogArticle.jsx
@@ -7,7 +7,7 @@ const BlogArticle = ({ blog }) => {
     return (
         <div className='w-full px-4 md:px-20 lg:px-32 grid place-items-center pt-32 pb-14'>
             <div className='md:flex md:flex-row-reverse'>
-                <div className='w-2/3 md:mr-auto'>
+                <div className='md:w-2/3 md:mr-auto'>
                     <h1 className='font-Raleway font-black text-zinc-700/95 text-xl tracking-wider leading-7'>
                         {blog.title}
                     </h1>
